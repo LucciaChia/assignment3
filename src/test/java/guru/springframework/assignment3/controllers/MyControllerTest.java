df8c42275a3e851855ee5cf1989746fd4c6e93aa
KrausováLucia <lucia.krausova@e-star.com>
2024-05-15 17:09:06 +0200

    fixing intelliJ warnings

--- src/test/java/guru/springframework/assignment3/controllers/MyControllerTest.java
+++ src/test/java/guru/springframework/assignment3/controllers/MyControllerTest.java
@@ -2,8 +2,6 @@ package guru.springframework.assignment3.controllers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MyControllerTest {
 
     @Test
