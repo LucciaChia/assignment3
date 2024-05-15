8e32169fe42bff08d6ab56f5cf9ad732f8a867af
KrausováLucia <lucia.krausova@e-star.com>
2024-05-15 18:46:12 +0200

    create and use English and Spanish profile

---
+++ src/main/java/guru/springframework/assignment3/services/i18n/SpanishGreetingService.java
@@ -0,0 +1,15 @@
+package guru.springframework.assignment3.services.i18n;
+
+import guru.springframework.assignment3.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18NService")
+public class SpanishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
